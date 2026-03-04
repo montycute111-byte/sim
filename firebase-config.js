2592160d7b1f03a040222e9971c8d5654dffa344
montycute111-byte <montycute111@gmail.com>
2026-03-03 22:39:39 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "bank-sim-e1116.firebaseapp.com",
-    projectId: "bank-sim-e1116",
-    storageBucket: "bank-sim-e1116.firebasestorage.app",
-    messagingSenderId: "1007081823271",
-    appId: "1:1007081823271:web:90174d4579f52204e2c202"
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCFMueRP7Ea86hQDEoeOE_PZEz7utR98ZU",
+    authDomain: "savefix-75b7a.firebaseapp.com",
+    projectId: "savefix-75b7a",
+    storageBucket: "savefix-75b7a.firebasestorage.app",
+    messagingSenderId: "357772767528",
+    appId: "1:357772767528:web:b1e9e469c9f526e362a01d"
 };
-
-window.FIREBASE_CONFIG = firebaseConfig;
