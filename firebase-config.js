97005798e596c94a6a6ce299b72370ee84775145
montycute111-byte <montycute111@gmail.com>
2026-03-03 21:55:37 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "bank-sim-e1116.firebaseapp.com",
+    projectId: "bank-sim-e1116",
+    storageBucket: "bank-sim-e1116.firebasestorage.app",
+    messagingSenderId: "1007081823271",
+    appId: "1:1007081823271:web:90174d4579f52204e2c202"
+};
+
+window.FIREBASE_CONFIG = firebaseConfig;
